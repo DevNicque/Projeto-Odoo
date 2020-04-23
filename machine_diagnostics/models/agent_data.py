d190e67f7bf665b3409f6d9962e695a05bab56f6
Petronilo Bion <petroniloneto.am@hotmail.com>
2020-04-23 16:33:02 -0300

    Add new rules

--- machine_diagnostics/models/agent_data.py
+++ machine_diagnostics/models/agent_data.py
@@ -28,17 +28,19 @@ class AgentData(models.Model):
     def get_agent_data(self, payload):
         for attr in payload:
 
+            # [word.lower() for word in s.split()]
+            
             if attr["age_attribute"] == "Memória SLT1 Capacidade":
-                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "Gb"
+                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "GB"
 
             elif attr["age_attribute"] == "Memória SLT1 Frequência":
-                age_attribute_value = str(attr["age_attribute_value"]) + "Mhz"
+                age_attribute_value = str(attr["age_attribute_value"]) + "MHz"
 
             elif attr["age_attribute"] == "Memória SLT2 Capacidade":
-                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "Gb"
+                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "GB"
 
             elif attr["age_attribute"] == "Memória SLT2 Frequência":
-                age_attribute_value = str(attr["age_attribute_value"]) + "Mhz"
+                age_attribute_value = str(attr["age_attribute_value"]) + "MHz"
 
             elif attr["age_attribute"] == "Tela Marca":
                 age_attribute_value = attr["age_attribute_value"].split('\\')[1][:-4]
@@ -46,18 +48,69 @@ class AgentData(models.Model):
             elif attr["age_attribute"] == "Tela Taxa de Atualização de Frame":
                 age_attribute_value = str(attr["age_attribute_value"]) + "Hz"
 
-            elif attr["age_attribute"] == "Placa de Vídeo Intel Memória Dedicada":
-                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "Gb"
+            elif attr["age_attribute"] == "Placa de Vídeo Nvidia Memória Dedicada":
+                # age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "GB"
 
-            #else if attr["age_attribute"] == "Placa de Vídeo Nvidia Memória Dedicada":
-            #    age_attribute_value = str(int(attr["age_attribute_value"])/1073741824) + "G"
+                modelo = attr["age_attribute_value"]
+                words = [word.lower() for word in modelo.split()]
+
+                if "geforce" in words and "gtx" in words and "1650" in words:
+                    age_attribute_value = "4GB"
+
+                elif "geforce" in words and "gtx" in words and "1050" in words:
+                    age_attribute_value = "3GB"
+
+                elif "geforce" in words and "gtx" in words and "1050" in words:
+                    age_attribute_value = "3GB"
+
+                elif "geforce" in words and "gtx" in words and "1660TI" in words:
+                    age_attribute_value = "6GB"
+
+                elif "geforce" in words and "rtx" in words and "2070" in words and "MAX-Q" in words:
+                    age_attribute_value = "8GB"
+
+                elif "geforce" in words and "rtx" in words and "2060" in words:
+                    age_attribute_value = "6GB"
+
+                elif "geforce" in words and "rtx" in words and "2070" in words:
+                    age_attribute_value = "8GB"
+
+                elif "geforce" in words and "rtx" in words and "2080" in words and "MAX-Q" in words:
+                    age_attribute_value = "8GB"
+
+                else:
+                    age_attribute_value = "NULL"
             #OBS: falta tratar o bug do uint32
 
+            elif attr["age_attribute"] == "Placa de Vídeo Intel Memória Dedicada":
+                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "GB"
+
             elif attr["age_attribute"] == "Armazenamento SSD Capacidade":
-                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "Gb"
+                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "GB"
+                #capacidade = int(int(attr["age_attribute_value"])/1073741824))
+                #if capacidade <= 500 and capacidade >= 400:
+                #    age_attribute_value = "500GB"
+                #elif capacidade <= 250 and capacidade >= 100:
+                #    age_attribute_value = "250GB"
+                #elif capacidade <= 1000 and capacidade > 750:
+                #    age_attribute_value = "1TB"
+                #elif capacidade <= 2000 and capacidade > 1750:
+                #    age_attribute_value = "2TB"
+                #else:
+                #    age_attribute_value = "NULL"
 
             elif attr["age_attribute"] == "Armazenamento HD Capacidade":
-                age_attribute_value = str(int(int(attr["age_attribute_value"])/1073741824)) + "Gb"
+                capacidade = int(int(attr["age_attribute_value"])/1073741824))
+                if capacidade <= 500 and capacidade >= 400:
+                    age_attribute_value = "500GB"
+                elif capacidade <= 250 and capacidade >= 100:
+                    age_attribute_value = "250GB"
+                elif capacidade <= 1000 and capacidade > 750:
+                    age_attribute_value = "1TB"
+                elif capacidade <= 2000 and capacidade > 1750:
+                    age_attribute_value = "2TB"
+                else:
+                    age_attribute_value = "NULL"
 
             elif attr["age_attribute"] == "Teclado Referência":
                 if attr["age_attribute_value"] == "00010416":
@@ -73,6 +126,15 @@ class AgentData(models.Model):
                 lista = attr["age_attribute_value"].split('] ')
                 lista.pop(0)
                 age_attribute_value = ''.join(lista)
+
+            elif attr["age_attribute"] == "Bios Data":
+                # 20190819000000.000000+000
+                # \'%Y-%m-%d %H:%M:%S\'
+                aux = attr["age_attribute_value"]
+                ano = aux[0:4]
+                mes = aux[4:6]
+                dia = aux[6:8]
+                age_attribute_value = ano + "-" + mes + "-" + dia
 
             else:
                 age_attribute_value = attr["age_attribute_value"]
