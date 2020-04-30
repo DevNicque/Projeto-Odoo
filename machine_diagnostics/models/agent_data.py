6cf4b4e299f12dccf933c36ded892c0e2dde0225
Petronilo Bion <petroniloneto.am@hotmail.com>
2020-04-30 17:00:33 -0300

    adfasdfasdf

--- machine_diagnostics/models/agent_data.py
+++ machine_diagnostics/models/agent_data.py
@@ -33,7 +33,7 @@ class AgentData(models.Model):
         # Passo para determinar o que existe no Odoo e que não existe no payload, sendo um componente "removido":
         register_agent = self.search([("name", "=", payload[0]["name"])])
         for register in register_agent:
-            if not((register["age_attribute"] in [x["age_attribute"] for x in payload]) and (register["age_devicesn"] in [x["age_devicesn"] for x in payload]) and (register["age_status"] == "Trocado")):
+            if not((register["age_attribute"] in [x["age_attribute"] for x in payload]) and not((register["age_devicesn"] in [x["age_devicesn"] for x in payload]) and (register["age_status"] != "Trocado"):
                 # somente caso o componente não tenha sido trocando antes.
                 register.sudo().write({"age_status": "Removido"})
             # componente removido/inativo
@@ -188,7 +188,7 @@ class AgentData(models.Model):
                     register_line.sudo().write({"age_status": "Trocado"})
 
                 if not((vals["age_deviceid"]) == False):
-                    # Precisa ocorrer após a verificação para o status de "troca"
+                    # Precisa ocorrer após a verificação do status de "troca"
                     self.sudo().create(vals)
         
         return True
